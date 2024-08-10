286d8e2edea1d3218b8a4de94b321e2579dc2762
koryruno <koryruno@yandex.ru>
2024-08-10 22:28:26 +0300

    initial project

---
+++ src/test/java/ru/koryruno/springbootstartert1/Springbootstartert1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.koryruno.springbootstartert1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootstartert1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
